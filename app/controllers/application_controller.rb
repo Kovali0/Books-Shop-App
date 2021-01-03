83c6499d654d2b5b6b2a8f440623cac88b85550f
Kovali0 <kowal.bartek.10@gmail.com>
2021-01-03 12:14:24 -0800

    [Add][User model][Devise][Navigation Menu] Add on the top of site the navigation menu. Install and setup devise gem. Connect devise root and create profile page for user model. Improvment user model by adding username field and modify login system with additional possibility to use username or email for authentication.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,19 @@
 class ApplicationController < ActionController::Base
-  def hello
-    render html: "hello, world!"
+
+  before_action :authenticate_user!
+
+  def mainpage
+    render "layouts/mainpage"
   end
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    added_attrs = [:username, :email, :password, :password_confirmation, :remember_me]
+    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  root 'application#hello'
+
+  devise_for :users, :path_prefix => 'd'
+  resources :users, :only =>[:show]
+
+  get 'users/index'
+  match '/users',   to: 'users#index',   via: 'get'
+  match '/users/:id',     to: 'users#show',       via: 'get'
+
+  root 'application#mainpage'
+
 end
